2c606ce806965c3b48c0507ffecdabb08cb0239f
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-04-10 22:53:18 +0900

    コメントといいね機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_120500) do
+ActiveRecord::Schema.define(version: 2020_04_10_131253) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
